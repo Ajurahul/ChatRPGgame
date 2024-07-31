daac9114463d1acb1529e5b23785b8557488d225
Ajurahul <48558382+Ajurahul@users.noreply.github.com>
2024-07-31 21:15:39 +0530

    initial commit of code

---
+++ src/main/java/com/chat/rpg/RpgGameApplication.java
@@ -0,0 +1,13 @@
+package com.chat.rpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chat/rpg/RpgGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.rpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
